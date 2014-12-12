77b145916b5cd411178c48b9ce7adcc5924bb038
Andreas Kloeckner <inform@tiker.net>
2014-12-11 18:23:07 -0600

    Start dagrt

---
+++ README.rst
@@ -0,0 +1,13 @@
+dagrt
+=====
+
+.. image:: https://badge.fury.io/py/dagrt.png
+    :target: http://pypi.python.org/pypi/dagrt
+
+dagrt will be a DAG-based run-time system for operator evaluation, capable of
+operator fusion and versatile code generation.
+
+Resources:
+
+* `wiki home page <http://wiki.tiker.net/Leap>`_
+* `source code via git <https://github.com/inducer/dagrt>`_
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    from setuptools import setup, find_packages
+
+    version_dict = {}
+    init_filename = "dagrt/version.py"
+    exec(compile(open(init_filename, "r").read(), init_filename, "exec"),
+            version_dict)
+
+    setup(name="dagrt",
+          version=version_dict["VERSION_TEXT"],
+          description="Time integration by code generation",
+          long_description=open("README.rst", "rt").read(),
+          author="Andreas Kloeckner",
+          author_email="inform@tiker.net",
+          license="MIT",
+          url="http://wiki.tiker.net/Leap",
+          classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Other Audience',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Natural Language :: English',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 2.6',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3.3',
+              'Programming Language :: Python :: 3.4',
+              'Topic :: Scientific/Engineering',
+              'Topic :: Scientific/Engineering :: Mathematics',
+              'Topic :: Software Development :: Libraries',
+              'Topic :: Utilities',
+              ],
+
+          packages=find_packages(),
+          install_requires=[
+              "numpy>=1.5",
+              "pytools>=2014.1",
+              "pymbolic>=2014.1",
+              "pytest>=2.3",
+              "six",
+              ],
+          )
+
+
+if __name__ == '__main__':
+    main()
