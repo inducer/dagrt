d40209c42e328e7d4bd3cad9b185c8732ee5a288
Matt Wala <wala1@illinois.edu>
2016-03-28 10:58:52 -0500

    Get rid of simple parser test (we have another more comprehensive one).

--- test/test_expressions.py
+++ test/test_expressions.py
@@ -61,12 +61,6 @@ def test_match():
     assert subst["t"] == tt
 
 
-def test_parse():
-    from pymbolic import var
-    from dagrt.expression import parse
-    assert parse("1 + `<dt>`") == 1 + var("<dt>")
-
-
 def test_get_variables():
     from pymbolic import var
     f = var('f')
