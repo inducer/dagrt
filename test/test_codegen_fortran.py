1381cf0b61a34f1cf5541862f383eac565c5c2f2
Cory Mikida <cmikida2@dunkel.cs.illinois.edu>
2017-09-21 10:54:00 -0500

    Remove outdated and ineffectual least squares test

--- test/test_codegen_fortran.py
+++ test/test_codegen_fortran.py
@@ -121,59 +121,6 @@ def test_arrays_and_linalg():
         fortran_options=["-llapack", "-lblas"])
 
 
-def test_least_squares():
-    from dagrt.function_registry import base_function_registry as freg
-
-    with CodeBuilder(label="primary") as cb:
-        cb("n", "4")
-        cb("nodes", "`<builtin>array`(n)")
-        cb("vdm", "`<builtin>array`(n*n)")
-        cb("identity", "`<builtin>array`(n*n)")
-        cb.fence()
-
-        cb("nodes[i]", "i/n",
-                loops=[("i", 0, "n")])
-        cb("identity[i]", "0",
-                loops=[("i", 0, "n*n")])
-        cb.fence()
-
-        cb("identity[i*n + i]", "1",
-                loops=[("i", 0, "n")])
-        cb("vdm[j*n + i]", "nodes[i]**j",
-                loops=[("i", 0, "n"), ("j", 0, "n")])
-
-        cb.fence()
-
-        cb("vdm_inverse", "`<builtin>lls`(vdm, identity, n, n)")
-        cb("myarray", "`<builtin>matmul`(vdm, vdm_inverse, n, n)")
-
-        cb("myzero", "myarray - identity")
-        cb((), "`<builtin>print`(myzero)")
-        with cb.if_("`<builtin>norm_2`(myzero) > 10**(-8)"):
-            cb.raise_(MatrixInversionFailure)
-
-    code = DAGCode.create_with_steady_state(
-        cb.state_dependencies, cb.instructions)
-
-    codegen = f.CodeGenerator(
-            'arrays',
-            function_registry=freg,
-            user_type_map={},
-            emit_instrumentation=True,
-            timing_function="second")
-
-    code_str = codegen(code)
-    if 0:
-        with open("arrays.f90", "wt") as outf:
-            outf.write(code_str)
-
-    run_fortran([
-        ("arrays.f90", code_str),
-        ("test_arrays_and_linalg.f90", read_file("test_arrays_and_linalg.f90")),
-        ],
-        fortran_options=["-llapack", "-lblas"])
-
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         exec(sys.argv[1])
